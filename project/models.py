8ab7e4ffbecb2c79043c87c9ee818b8f21c3f80b
vomit <rvotaisrvota@gmail.com>
2022-09-07 16:05:50 +0300

    Initial commit

---
+++ project/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from marshmallow import Schema, fields
+from sqlalchemy.orm import relationship
+
+from project.setup.db import models
+
+
+class Genre(models.Base):
+    __tablename__ = 'genres'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Director(models.Base):
+    __tablename__ = 'directors'
+
+    name = Column(String(100), unique=True, nullable=False)
+
+
+class Movie(models.Base):
+    __tablename__ = 'movies'
+
+    title = Column(String(100), nullable=False)
+    description = Column(String(250))
+    trailer = Column(String(250))
+    year = Column(Integer, nullable=False)
+    rating = Column(Float, nullable=False)
+    genre_id = Column(Integer, ForeignKey(f"{Genre.__tablename__}.id"), nullable=False)
+    director_id = Column(Integer, ForeignKey(f"{Director.__tablename__}.id"), nullable=False)
+    genre = relationship('Genre')
+    director = relationship('Director')
+
+
+class User(models.Base):
+    __tablename__ = 'users'
+
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(300), nullable=False)
+    name = Column(String(100))
+    surname = Column(String(100))
+    favorite_genre = Column(Integer, ForeignKey(f"{Director.__tablename__}.id"))
---
+++ project/setup/api/models.py
@@ -0,0 +1,35 @@
+from flask_restx import fields, Model
+
+from project.setup.api import api
+
+genre: Model = api.model('Жанр', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+})
+
+
+director: Model = api.model('Режиссеры', {
+    'id': fields.Integer(required=True, example=1),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+})
+
+movie: Model = api.model('Фильмы', {
+    'id': fields.Integer(required=True, example=1),
+    'title': fields.String(required=True, max_length=100, example='Комедия'),
+    'description': fields.String(required=True, max_length=100, example='Комедия'),
+    'trailer': fields.String(required=True, max_length=100, example='Комедия'),
+    'year': fields.Integer(required=True, max_length=100, example='Комедия'),
+    'rating': fields.Float(required=True, max_length=100, example='Комедия'),
+    'genre': fields.Nested(genre),
+    'director': fields.Nested(director),
+})
+
+user: Model = api.model('Пользователи', {
+    'id': fields.Integer(required=True, example=1),
+    'email': fields.String(required=True, example=1),
+    'password': fields.String(required=True, max_length=100, example='Комедия'),
+    'name': fields.String(required=True, max_length=100, example='Комедия'),
+    'surname': fields.String(required=True, max_length=100, example='Комедия'),
+    'favorite_genre': fields.Integer(required=True, max_length=100, example='Комедия'),
+
+})
